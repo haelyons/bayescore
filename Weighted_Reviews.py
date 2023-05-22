d862820c9486485f86c88d16e693f4572e390e71
Helios Lyons <helioslyons@me.com>
2023-05-22 19:58:35 +0100

    Added further examples, update todo

--- Weighted_Reviews.py
+++ Weighted_Reviews.py
@@ -17,11 +17,26 @@
 # Assume you have W ratings of value R along with any user ratings, and find the average. 
 # Given R=2 and W=3, where N is number of user ratings, V is their average
 #
-# 1. For 100 user ratings of 4 [ (R*W + N*V) / (W+N)]
-# ∴ (2*3 + 100*4) / (100 + 3) = 3.94☆
-# In comparison, using normal averaging would give us a 4☆ rating.
+# Weighted_Rating = (R*W + N*V) / (W+N)
 #
-# TODO Further examples (3 more written)
+# 1. For 100 user ratings of 4 
+# ∴ Weighted_Rating = (2*3 + 100*4) / (100 + 3) = 3.94☆
+# In comparison, a straight average would give us a 4☆ rating.
+#
+# 2. For 3 ratings of 5 and 1 rating of 4
+# ∴ Weighted_Rating = (2*3 + 3*5 + 4) / (3 + 4) = 3.57☆
+# Average = 4.75☆
+#
+# 3. For 10 ratings of 4
+# ∴ Weighted_Rating = (2*3 + 10*4) / (3 + 10) = 3.54☆
+# Average = 4☆
+#
+# 4. For 1 ratings of 5
+# ∴ Weighted_Rating = (2*3 + 5) / (3 + 1) = 2.75☆
+# Average = 5☆
+#
 # TODO Actual implementation of the base function
 # TODO Function to create synthetic review data
 # TODO Importation of actual review data from UberEats or Amazon :)
+# TODO Find the related XKCD about how anything below 5 starts is shit
+# TODO Transcribe rationale for this program from notes
