c0f0f41794e935c3ac1e54a01f174fe9cd269d27
haelyons <helioslyons@me.com>
2023-05-26 14:17:36 +0100

    Remove matplotlib, wrong tool for the job

--- Weighted_Reviews.py
+++ Weighted_Reviews.py
@@ -41,13 +41,6 @@ def randomList():
 def compute_rating(r, w, g_mean, g_len):
     return ((r * w) + (g_mean * g_len)) / (w + g_len)
 
-def recursive_compute(r, w, g_mean, g_len, list):
-    tempRating = 0
-    currentRating = (currentRating + tempRating) / 2
-    for rating in list:
-        
-
-
 list = ex1 # Select list
 
 rating_avg = statistics.mean(list)
@@ -61,55 +54,3 @@ print(rating)
 # is where we see significant differences in rating schemas. Over 1000 ratings we 
 # can see that there is very little difference between the weighted rating, and 
 # the mean rating.
-
-
-# Create the figure and the line that we will manipulate
-n = np.linspace(0,len(list), len(list))
-fig, ax = plt.subplots()
-line, = ax.plot(n, compute_rating(r, w, rating_avg, len(list)), lw=2)
-
-# Adjust the main plot to make room for the sliders
-fig.subplots_adjust(left=0.25, bottom=0.25)
-
-ax.set_xlabel('Reviews [n]')
-
-# Make a horizontal slider to control the initial rating.
-r_ax = fig.add_axes([0.25, 0.1, 0.65, 0.03])
-r_slider = Slider(
-    ax=r_ax,
-    label='Initial Rating',
-    valmin=0,
-    valmax=10,
-    valinit=r,
-)
-
-# Make a vertically oriented slider to control the weight of the initial rating
-w_ax = fig.add_axes([0.1, 0.25, 0.0225, 0.63])
-w_slider = Slider(
-    ax=w_ax,
-    label="Weight of Initial Rating",
-    valmin=0,
-    valmax=10,
-    valinit=w,
-    orientation="vertical"
-)
-
-# The function to be called anytime a slider's value changes
-def update(val):
-    line.set_ydata(compute_rating(r_slider, w_slider, rating_avg, len(list)))
-    fig.canvas.draw_idle()
-
-# register the update function with each slider
-r_slider.on_changed(update)
-w_slider.on_changed(update)
-
-# Create a `matplotlib.widgets.Button` to reset the sliders to initial values.
-resetax = fig.add_axes([0.8, 0.025, 0.1, 0.04])
-button = Button(resetax, 'Reset', hovercolor='0.975')
-
-def reset(event):
-    r_slider.reset()
-    w_slider.reset()
-button.on_clicked(reset)
-
-plt.show()
