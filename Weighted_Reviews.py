934e148662cd18de6f532a4d87aad6cbd16a4915
haelyons <helioslyons@me.com>
2023-05-26 14:28:54 +0100

    Sketch of a tkinter GUI to modify value live

--- Weighted_Reviews.py
+++ Weighted_Reviews.py
@@ -1,5 +1,6 @@
 import statistics
 import random
+import tkinter as tk
 
 # RELATED XKCD
 # https://www.explainxkcd.com/wiki/index.php/1098:_Star_Ratings
@@ -8,6 +9,9 @@ import random
 # their effects on the weighted rating - use matplotlib
 # TODO Importation of actual review data from UberEats or Amazon :)
 # TODO Plot results for shorter vs. longer datasets and similarity with mean
+
+global r
+global w
 
 r = 2
 w = 3
@@ -51,3 +55,48 @@ print(rating)
 # is where we see significant differences in rating schemas. Over 1000 ratings we 
 # can see that there is very little difference between the weighted rating, and 
 # the mean rating.
+
+def update_rating():
+    list = ex1  # Select list
+
+    rating_avg = statistics.mean(list)
+    rating = compute_rating(r, w, rating_avg, len(list))
+    rating_label.config(text=f"Rating: {rating}")
+
+def on_r_changed(event):
+    new_r = int(r_entry.get())
+    global r 
+    r = new_r
+    update_rating()
+
+def on_w_changed(event):
+    new_w = int(w_entry.get())
+    global w
+    w = new_w
+    update_rating()
+
+root = tk.Tk()
+root.title("Rating Calculator")
+
+r_label = tk.Label(root, text="R:")
+r_label.grid(row=0, column=0, padx=5, pady=5)
+
+r_entry = tk.Entry(root, width=5)
+r_entry.insert(tk.END, str(r))
+r_entry.grid(row=0, column=1, padx=5, pady=5)
+r_entry.bind("<Return>", on_r_changed)
+
+w_label = tk.Label(root, text="W:")
+w_label.grid(row=1, column=0, padx=5, pady=5)
+
+w_entry = tk.Entry(root, width=5)
+w_entry.insert(tk.END, str(w))
+w_entry.grid(row=1, column=1, padx=5, pady=5)
+w_entry.bind("<Return>", on_w_changed)
+
+rating_label = tk.Label(root, text="Rating: ")
+rating_label.grid(row=2, column=0, columnspan=2, padx=5, pady=5)
+
+update_rating()
+
+root.mainloop()
